1daaeb642404b3634ee970482df2538f94ae8e37
Vincent Vandenbussche <vandenbussche.vincent@gmail.com>
2024-12-29 17:26:27 +0100

    Add params to segmentation demo

--- blender_segmentation/run_segmentation_demo.py
+++ blender_segmentation/run_segmentation_demo.py
@@ -1,3 +1,5 @@
+import argparse
+
 import cv2
 import torch
 import numpy as np
@@ -20,47 +22,41 @@ def preprocess_image(image):
     
     return image_tensor.unsqueeze(0)  # Add batch dimension
 
-def run_demo():
+def run_demo(args):
     # Load the trained model
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model = smp.Unet(
-        encoder_name="timm-mobilenetv3_large_100",
+        encoder_name=args.encoder,
         encoder_weights=None,
         in_channels=3,
         classes=1,
         activation='sigmoid',
     )
-    model.load_state_dict(torch.load('trained_model.pth', map_location=device))
+    model.load_state_dict(torch.load(args.model_path, map_location=device))
     model.to(device)
     model.eval()
 
     # Open webcam
     cap = cv2.VideoCapture(0)
-
     while True:
         ret, frame = cap.read()
         if not ret:
             break
-
         # Preprocess the frame
         input_tensor = preprocess_image(frame)
         input_tensor = input_tensor.to(device)
-
         # Run inference
         with torch.no_grad():
             mask = model(input_tensor)
             mask = mask.squeeze().cpu().numpy()
         # Resize mask to match the original frame size
         mask_resized = cv2.resize(mask, (frame.shape[1], frame.shape[0]))
-
         # Create a colored overlay
         overlay = np.zeros_like(frame)
         overlay[mask_resized > 0.5] = [0, 255, 0]  # Green color for segmented areas
-
         # Blend the original frame with the overlay
         alpha = 0.2
         cv2.addWeighted(overlay, alpha, frame, 1 - alpha, 0, frame)
-
         # Display the result
         cv2.imshow('Hand segmentation', frame)
 
@@ -71,4 +67,9 @@ def run_demo():
     cv2.destroyAllWindows()
 
 if __name__ == "__main__":
-    run_demo()
+    parser = argparse.ArgumentParser(description="Run the semantic segmentation model on webcam")
+    parser.add_argument("--model_path", type=str, default="model.pt", help="Path to the model ")
+    parser.add_argument("--encoder", type=str, default="timm-mobilenetv3_large_100", help="Encoder architecture")
+
+    args = parser.parse_args()
+    run_demo(args)
